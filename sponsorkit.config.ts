5613281260430096b16adb7d8496b1248d9a907a
Haowei Hsu <hwhsu1231@gmail.com>
2024-12-02 00:22:20 +0800

    test

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -21,32 +21,38 @@ export default defineConfig({
     },
     {
       title: 'Sponsors',
+      preset: presets.xs,
+    },
+    {
+      title: 'Bronze Sponsors',
       monthlyDollars: 10,
-      preset: {
-        avatar: {
-          size: 42,
-        },
-        boxWidth: 52,
-        boxHeight: 52,
-        container: {
-          sidePadding: 30,
-        },
-      }
+      preset: presets.small,
     },
     {
       title: 'Silver Sponsors',
       monthlyDollars: 40,
-      preset: presets.medium,
+      preset: presets.base,
     },
     {
       title: 'Gold Sponsors',
       monthlyDollars: 160,
-      preset: presets.large,
+      preset: presets.medium,
     },
     {
       title: 'Platinum Sponsors',
       monthlyDollars: 640,
+      preset: presets.large,
+    },
+    {
+      title: 'Diamond Sponsors',
+      monthlyDollars: 2560,
       preset: presets.xl,
     }
-  ]
+  ],
+
+
+  formats: ['svg', 'png'],
+  afdian: {
+    exechangeRate: 4,
+  },
 })
